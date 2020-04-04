51587e1d1b103bc9d9991634048de0706b2535f4
Koshkens <33977229+Koshkens@users.noreply.github.com>
2020-04-04 16:56:45 +0300

    Updates

---
+++ precache-manifest.e2f2d3f22c24e43071e2f334f0567994.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c725849aef1900774473cdbab0511f3",
+    "url": "/vk-mini-apps-space-gray/index.html"
+  },
+  {
+    "revision": "f174b4a43b3661305ae0",
+    "url": "/vk-mini-apps-space-gray/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "141b2d6a1a503cf3a32e",
+    "url": "/vk-mini-apps-space-gray/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "f174b4a43b3661305ae0",
+    "url": "/vk-mini-apps-space-gray/static/js/2.e00aeb91.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/vk-mini-apps-space-gray/static/js/2.e00aeb91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "141b2d6a1a503cf3a32e",
+    "url": "/vk-mini-apps-space-gray/static/js/main.8a29197b.chunk.js"
+  },
+  {
+    "revision": "8b4851130558914e4a27",
+    "url": "/vk-mini-apps-space-gray/static/js/runtime-main.f6f286a7.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/vk-mini-apps-space-gray/static/media/persik.4e1ec840.png"
+  }
+]);
